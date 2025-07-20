9c33daf3d5dd2fd6ee42e92fb9aa51b4d1773848
jess <jesswillcode@gmail.com>
2025-07-19 22:05:37 -0400

    changed state from using context to using useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { MealProvider } from './context/MealContext'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import MealList from './components/MealList'
@@ -11,24 +9,22 @@ import './App.css'
 
 function App() {
   return (
-    <MealProvider>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <main className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/meals" element={<MealList />} />
-              <Route path="/meals/new" element={<MealForm />} />
-              <Route path="/meals/edit/:id" element={<MealForm />} />
-              <Route path="/users" element={<UserList />} />
-              <Route path="/users/new" element={<UserForm />} />
-              <Route path="/users/edit/:id" element={<UserForm />} />
-            </Routes>
-          </main>
-        </div>
-      </Router>
-    </MealProvider>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/meals" element={<MealList />} />
+            <Route path="/meals/new" element={<MealForm />} />
+            <Route path="/meals/edit/:id" element={<MealForm />} />
+            <Route path="/users" element={<UserList />} />
+            <Route path="/users/new" element={<UserForm />} />
+            <Route path="/users/edit/:id" element={<UserForm />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   )
 }
 
